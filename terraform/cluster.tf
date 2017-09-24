87b89d5ca006d3749cbc06477b0eeb9217667e4a
Mykyta Protsenko <mykyta@protsenko.com>
2017-09-24 16:38:00 -0700

    Updated project, set up demo cluster

---
+++ terraform/cluster.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "gcs" {
+    bucket = "grpcvsrestdemo-terraform-state"
+    path = "cluster-terraform.tfstate"
+    project = "grpcvsrestdemo"
+  }
+}
+
+
+provider "google" {
+  project = "alien-fold-180922"
+  region = "us-west1"
+}
+
+resource "google_container_cluster" "demo_cluster" {
+  name = "k8s-cluster"
+  zone = "us-west1-a"
+  initial_node_count = 2
+
+  master_auth {
+    username = "${var.cluster_auth_user}"
+    password = "${var.cluster_auth_pwd}"
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+/*
+resource "google_project_services" "rgrpcvsrest" {
+  project = "rgrpcvsrest"
+  services = [
+    "container.googleapis.com",
+    "compute.googleapis.com"
+  ]
+}
+
+*/
